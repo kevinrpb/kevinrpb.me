70c5abfe03aff7108da6e1244f2603ef84b97915
Kevin Romero Peces-Barba <kevinrpb@hotmail.com>
2020-08-19 18:40:03 +0200

    Create mailobfuscator.js

---
+++ assets/js/mailobfuscator.js
@@ -0,0 +1,17 @@
+const mail_coded = "p5WXLv3Y@lybwSXA.Vyw"
+const mail_key = "vudUoR3mBzKEhn2ca0GDWt46yO1wkiJelLPVSNHC9brsjxFMpXZ5A7fT8QIqgY"
+const mail_shift = coded.length
+
+let mail_link = ""
+let mail_ltr = ""
+
+for (let i = 0; i < mail_coded.length; i++) {
+  if (mail_key.indexOf(mail_coded.charAt(i)) == -1) {
+    mail_ltr = mail_coded.charAt(i)
+    mail_link += (ltr)
+  }
+  else {     
+    mail_ltr = (mail_key.indexOf(mail_coded.charAt(i)) - mail_shift + mail_key.length) % mail_key.length
+    mail_link += (mail_key.charAt(ltr))
+  }
+}
