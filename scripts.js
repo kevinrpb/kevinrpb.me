4f3d1cfe245d8c0402dbdc44e3d49fe23c538ad8
kevinrpb <kevinrpb@hotmail.com>
2021-11-26 11:45:53 +0000

    Deploying to gh-pages from @ kevinrpb/kevinrpb.me@a2a919ab2aeb52fabda981bb0665474f1f8058a3 🚀

--- scripts.js
+++ scripts.js
@@ -326,9 +326,11 @@ function addShootingStars(min, max, p) {
   }
 }
 
-function init() {
-  setMailById('mail_card', true, false)
-  // setMailById('mail_reach', true, true)
+function init(with_mail = true) {
+  if (with_mail) {
+    setMailById('mail_card', true, false)
+    // setMailById('mail_reach', true, true)
+  }
 
   setupParticleCanvas()
 }
