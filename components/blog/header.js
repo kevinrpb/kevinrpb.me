9a5d2dfa121277cb409efd1b88f87cc72fe7ecd9
Kevin Romero Peces-Barba <hello@kevinrpb.me>
2022-05-12 12:36:17 +0200

    Setup header styling

--- components/blog/header.js
+++ components/blog/header.js
@@ -1,11 +1,18 @@
 import React from 'react'
 
 import SaneLink from '@components/link'
+import Icon from '@components/icon'
 
 const BlogHeader = (props) => (
   <header {...props}>
+    <SaneLink href='/' className=''>
+      <Icon name='home' />
+      Home
+    </SaneLink>
+
     <nav>
       <SaneLink href='/blog' className=''>
+        <Icon name='blog' />
         Blog
       </SaneLink>
     </nav>
--- pages/cv.js
+++ pages/cv.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 import Page from '@layouts/page'
 import SaneLink from '@components/link'
+import Icon from '@components/icon'
 import Resume from '@components/resume'
 
 import cv from '@data/cv.json'
@@ -9,13 +10,19 @@ import cv from '@data/cv.json'
 const CV = () => (
   <Page>
     <header data-page='cv'>
+      <SaneLink href='/' className=''>
+        <Icon name='home' />
+        Home
+      </SaneLink>
+
       <SaneLink
         href='/files/kevinrpb_cv.pdf'
         className='pill percentage-100 shadow'
         target='_blank'
         rel='noopener noreferrer'
       >
-        Download Resume
+        <Icon name='download' />
+        Download
       </SaneLink>
     </header>
 
