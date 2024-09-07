8909e8b0ffed885d6e3d52f80bb7e094eb1bf66d
Kevin Romero Peces-Barba <hello@kevinrpb.me>
2024-09-07 12:39:22 -0400

    Setup index page content

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,27 @@
-// @ts-check
 import { defineConfig } from 'astro/config'
 
+import tailwind from '@astrojs/tailwind'
+import vercel from '@astrojs/vercel/serverless'
+
+import icon from 'astro-icon'
+
 // https://astro.build/config
-export default defineConfig({})
+export default defineConfig({
+	site: 'https://kevinrpb.me',
+	output: 'hybrid',
+	adapter: vercel({
+		webAnalytics: {
+			enabled: true,
+		},
+	}),
+	integrations: [
+		tailwind(),
+		icon({
+			iconDir: 'src/assets/icons',
+			include: {
+				cbi: ['github', 'mail-ru', 'mastodon'],
+				'material-symbols': ['lab-profile-outline-rounded'],
+			},
+		}),
+	],
+})
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,10 +1,31 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			backgroundImage: {
+				'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+				'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+			},
+			colors: {
+				'ebony-clay': '#2a2c41',
+				'space-cadet': '#2a2c41',
+				manatee: '#8e9aaf',
+				'alice-blue': '#ecf1f3',
+				'imperial-red': '#ef253c',
+				'amaranth-red': '#d70427',
+				'blue-haze': '#c4c6d9',
+			},
+			gridTemplateColumns: {
+				'home-card': '2fr 5fr',
+			},
+			gridTemplateRows: {
+				'home-card': 'auto 1fr 1fr auto',
+			},
+			screens: {
+				xs: '400px',
+			},
+		},
 	},
 	plugins: [require('@tailwindcss/typography')],
 }
