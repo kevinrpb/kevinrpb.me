75ee7f0184204d190670e3a3b6017de3757356b5
Kevin Romero Peces-Barba <hello@kevinrpb.me>
2025-05-18 22:35:33 -0400

    Upgrade dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,34 +2,42 @@ import { defineConfig } from 'astro/config'
 
 import icon from 'astro-icon'
 import react from '@astrojs/react'
-import tailwind from '@astrojs/tailwind'
 import vercel from '@astrojs/vercel'
 
-import mdx from '@astrojs/mdx';
+import tailwindcss from '@tailwindcss/vite'
+
+import mdx from '@astrojs/mdx'
 
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://kevinrpb.me',
-    output: 'static',
-    trailingSlash: 'never',
-    adapter: vercel({
-        webAnalytics: {
-            enabled: true,
-        },
-    }),
-    integrations: [icon({
-        iconDir: 'src/assets/icons',
-        include: {
-            cbi: ['github', 'mail-ru', 'mastodon'],
-            'material-symbols': [
-                'link',
-                'lab-profile-outline-rounded',
-                'alternate-email-rounded',
-                'globe',
-                'download',
-                'arrow-left-alt-rounded',
-            ],
-            mdi: ['briefcase-outline', 'academic-cap-outline', 'heart-outline'],
-        },
-		}), react(), tailwind(), mdx()],
+	site: 'https://kevinrpb.me',
+	output: 'static',
+	trailingSlash: 'never',
+	adapter: vercel({
+		webAnalytics: {
+			enabled: true,
+		},
+	}),
+	vite: {
+		plugins: [tailwindcss()],
+	},
+	integrations: [
+		icon({
+			iconDir: 'src/assets/icons',
+			include: {
+				cbi: ['github', 'mail-ru', 'mastodon'],
+				'material-symbols': [
+					'link',
+					'lab-profile-outline-rounded',
+					'alternate-email-rounded',
+					'globe',
+					'download',
+					'arrow-left-alt-rounded',
+				],
+				mdi: ['briefcase-outline', 'academic-cap-outline', 'heart-outline'],
+			},
+		}),
+		react(),
+		mdx(),
+	],
 })
